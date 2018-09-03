b8702d79624613a5fdebf0f175403f02c96261fb
Jason Gallagher <grazinpalm@gmail.com>
2018-09-03 17:38:39 -0400

    Fixed bug with uri helper, Added Form class, Added logic for scraping forms from a response and add it to the response document. Added additional anglesharp attribute wrappers for easy access to common document attributes.

---
+++ BrowseSharpTest/Form.Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using BrowseSharp;
+using BrowseSharp.Html;
+using NUnit.Framework;
+
+namespace BrowseSharpTest
+{
+    [TestFixture]
+    public class FormTest
+    {
+        /* RequestTester Configuration */
+        public static int RequestTesterPort = 3000; // This is the port your RequestTester application is listening to
+        public static string RequestTesterRouteUri = "https://requesttester.com/tester/view" ;//"http://localhost:" + RequestTesterPort + "/tester/view";
+        
+        [Test]
+        public void TestExecute()
+        {
+            Browser browser = new Browser();
+            browser.Navigate("https://www.browsesharp.org/testsitesforms.html");
+            List<Form> forms = browser.Document.Forms;
+            var form = forms[0];
+            
+            form.SetValue("UserName", "TestUserName");
+            form.SetValue("Password", "TestPassword");
+            Assert.True(form.FormValues.ContainsKey("UserName"));
+            Assert.True(form.FormValues["UserName"] == "TestUserName");
+            Assert.True(form.FormValues.ContainsKey("Password"));
+            Assert.True(form.FormValues["Password"] == "TestPassword");
+        }
+
+    }
+}
