862f6e71958f725c4f43e063185705debee29339
Jayx239 <grazinpalm@gmail.com>
2018-10-18 20:57:18 -0400

    Added some javascript stuff

---
+++ BrowseSharp/Javascript/JavascriptEvalEngine.cs
@@ -0,0 +1,62 @@
+﻿using Jint;
+using Jint.Parser;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BrowseSharp.Javascript
+{
+    public class JavascriptEvalEngine
+    {
+        public JavascriptEvalEngine():base()
+        {
+
+        }
+        int index = 0;
+        public  Object EvaluateScript(IDocument document)
+        {
+            index = 0;
+            Engine engine = new Engine();
+            engine.SetValue("document", document.HtmlDocument);
+            var scripts = "";
+            //var variables = new Dictionary<string, string>();
+            foreach (var script in document.Scripts)
+            {
+                var scriptContent = script.Content;
+                if (script.Content.Contains("<")) {
+                    var variables = ReplaceDomStrings(script.Content,out scriptContent);
+                    foreach (var variable in variables)
+                    {
+                        engine.SetValue(variable.Key, variable.Value);
+                    }
+                }
+                scripts += scriptContent + "\n";
+            }
+            
+
+            ParserOptions options = new ParserOptions();
+            options.Tolerant = true;
+            return engine.Execute(scripts, options).GetCompletionValue().ToObject();
+        }
+        private Dictionary<string,string> ReplaceDomStrings(string script, out string outScript)
+        {
+            outScript = script;
+            var matches = GetDomStrings(script);
+            
+            Dictionary<string, string> variables = new Dictionary<string, string>();
+            foreach(var match in matches)
+            { var variableKey = "_scriptVar" + index++;
+                variables.Add(variableKey, match.ToString());
+                outScript = outScript.Replace(match.ToString(), variableKey);
+            }
+            return variables;
+        }
+
+        private MatchCollection GetDomStrings(string script)
+        {
+            Regex elementsRegex = new Regex("(\"<.*/>\")|(\"<.*>.*</.*>\")");
+            return elementsRegex.Matches(script);
+        }
+    }
+}
---
+++ BrowseSharpTest/JavascriptEngine.Test.cs
@@ -0,0 +1,34 @@
+﻿using BrowseSharp;
+using BrowseSharp.Javascript;
+using Jint;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrowseSharpTest
+{
+    [TestFixture]
+    public class JavascriptEngine
+    {
+        [Test]
+        public void Test()
+        {
+            Browser browser = new Browser();
+            IDocument document = browser.Navigate("www.browsesharp.com");
+            browser.JavascriptEngine.Document = document.Scripts[0].Content;
+            var value = browser.JavascriptEngine.Execute("$(document).html()");
+        }
+        [Test]
+        public void Test2()
+        {
+            Browser browser = new Browser();
+            IDocument document = browser.Navigate("http://browsesharp.org/testsitesjqueryrender.html");
+            browser.JavascriptEngine.Document = document.Scripts[0].Content;
+            JavascriptEvalEngine jsEngine = new JavascriptEvalEngine();
+            var result = jsEngine.EvaluateScript(document);
+        }
+    }
+}
